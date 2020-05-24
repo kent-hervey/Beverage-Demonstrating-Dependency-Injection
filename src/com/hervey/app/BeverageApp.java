5d08b77e8282c60e7f0aee3b60cf531916e80640
Kent Hervey <kent.hervey@gmail.com>
2020-05-24 09:07:37 -0500

    added argument to accommodate new BeverageService logic

--- src/com/hervey/app/BeverageApp.java
+++ src/com/hervey/app/BeverageApp.java
@@ -5,7 +5,7 @@ public class BeverageApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Beverage beverage = BeverageService.processBeverage();
+		Beverage beverage = BeverageService.processBeverage(false); //use false to indicate not a fun beverage
 		
 		System.out.println("returned beverage object is:  " + beverage);
 
