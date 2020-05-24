a492beca60847da305b23b9fcbce3d9a85446c43
Kent Hervey <kent.hervey@gmail.com>
2020-05-24 10:39:37 -0500

    Creation of ErrorBeverage class

---
+++ src/com/hervey/app/ErrorBeverage.java
@@ -0,0 +1,19 @@
+package com.hervey.app;
+
+public class ErrorBeverage implements Beverage {
+	//tapwater is free and comes with no container...just water and whatever minerals came from the
+	
+	public  ErrorBeverage() {
+		System.out.println("Reached in error; errorBeverage created");
+	}
+
+	@Override
+	public boolean satisfyThirst() {
+		System.out.println("TapWater satisfies thirs");
+		return false;
+	}
+
+	public boolean resealableContainer = false;
+	
+	
+}
