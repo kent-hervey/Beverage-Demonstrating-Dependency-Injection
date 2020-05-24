8c8e022c128f9f3fd87df8b4848101ce6262998b
Kent Hervey <kent.hervey@gmail.com>
2020-05-24 12:58:55 -0500

    output print statement indicates whether resealable

--- src/com/hervey/app/BeverageApp.java
+++ src/com/hervey/app/BeverageApp.java
@@ -8,6 +8,7 @@ public class BeverageApp {
 		Beverage beverage = BeverageService.processBeverage(false); //use false to indicate not a fun beverage
 		
 		System.out.println("returned beverage object is:  " + beverage);
+		System.out.println("This beverage has resealable container:    " + beverage.isItResealable());
 
 	}
 
