861d02a4f39a2de6d4bf7db532b54a4a574704ce
Kent Hervey <kent.hervey@gmail.com>
2020-05-24 10:40:52 -0500

    corrected boolean logic for TapWater selection

--- src/com/hervey/app/BeverageMaker.java
+++ src/com/hervey/app/BeverageMaker.java
@@ -2,13 +2,34 @@ package com.hervey.app;
 
 public class BeverageMaker {
 	
+
+	
+	
+	
+	
 	public static Beverage pickCreateBeverage(boolean alcohol, boolean flavoring, boolean supersweet, boolean carbonated) {
 		Beverage beverage;
-		if(!alcohol && !flavoring && !supersweet && carbonated) {
+		
+		if(!alcohol && !flavoring && !supersweet && !carbonated) {
 			beverage = new TapWater();
 		}
+
+//		else if(!alcohol && flavoring && !supersweet && !carbonated) {
+//			beverage = new DrPepper();
+//			
+//			
+//			
+//		}
+		
+		
 		else {
+			
+			
+			
 			beverage = new ErrorBeverage();
+			
+			
+			
 		}
 		
 		
