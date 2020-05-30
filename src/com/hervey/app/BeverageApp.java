01772134ef74c593c7747f3f4b6db6acf6d170e2
Kent Hervey <kent.hervey@gmail.com>
2020-05-30 16:21:20 -0500

    added userID and isPortable to main

--- src/com/hervey/app/BeverageApp.java
+++ src/com/hervey/app/BeverageApp.java
@@ -5,10 +5,23 @@ public class BeverageApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Beverage beverage = BeverageService.processBeverage(false); //use false to indicate not a fun beverage
+		int userID;
+		boolean isFun;
+		boolean isPortable;
+		
+		//simulates data from user interface for first user
+		userID=5;
+		isFun = true;
+		isPortable = false;
+		
+		Beverage beverage = BeverageService.processBeverage(isFun, userID, isPortable); //use false to indicate not a fun beverage
 		
 		System.out.println("returned beverage object is:  " + beverage);
 		System.out.println("This beverage has resealable container:    " + beverage.isItResealable());
+		
+		
+		
+		
 
 	}
 
